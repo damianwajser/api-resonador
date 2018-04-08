d6293cd0a098d64d46bb021ca9ca6d419e05c585
damianwajser <wajser@gmail,com>
2018-04-08 15:52:11 -0300

    datasource

--- src/main/java/com/example/myapp/DatabaseConfig.java
+++ src/main/java/com/example/myapp/DatabaseConfig.java
@@ -1,19 +1,11 @@
 package com.example.myapp;
 
-import javax.sql.DataSource;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DatabaseConfig {
-	@Bean
-	@Primary
-	@ConfigurationProperties(prefix = "spring.datasource")
-	public DataSource dataSource() {
-		return DataSourceBuilder.create().build();
-	}
+	Logger log = LoggerFactory.getLogger(getClass());
+
 }
--- src/main/java/com/example/myapp/Temperatura.java
+++ src/main/java/com/example/myapp/Temperatura.java
@@ -1,9 +1,13 @@
 package com.example.myapp;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Temperatura {
+	@Id
+	@GeneratedValue
 	private Long id;
 
 	public Long getId() {
