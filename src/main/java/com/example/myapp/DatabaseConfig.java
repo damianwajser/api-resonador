1e676b5b99aa1f83fe521316315cb573622f46df
damianwajser <wajser@gmail,com>
2018-04-08 18:57:47 -0300

    datasource

--- src/main/java/com/example/myapp/DatabaseConfig.java
+++ src/main/java/com/example/myapp/DatabaseConfig.java
@@ -4,7 +4,6 @@ import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
