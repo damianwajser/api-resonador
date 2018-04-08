c4f8aca531b8810ff6011fc2c2aba719c781a4a7
damianwajser <wajser@gmail,com>
2018-04-08 18:06:38 -0300

    algo

--- src/main/java/com/example/myapp/DemoApplication.java
+++ src/main/java/com/example/myapp/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.myapp;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,17 +12,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @SpringBootApplication
 public class DemoApplication {
-
+	
 	@Autowired
 	private TemperaturaRespository repository;
 
 	@RequestMapping("/")
 	@ResponseBody
-	public Iterable<Temperatura> home() {
+	public Iterable<Temperatura> home() throws InterruptedException {
+		for (int i = 0; i < 100; i++) {
+			Thread.sleep(10000l);
+			System.out.println("insertando" + new Date());
+			repository.save(new Temperatura(i));
+		}
 		return repository.findAll();
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
 	}
 }
